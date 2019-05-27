048cd651a5e02edaea37d796490d9a3ebf18fe9b
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-27 15:18:20 +0000

    feature: Import and add CardContainer component

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -6,7 +6,7 @@ import CardContainer from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
-    <div>
+    <div className='app-main'>
       <HeaderContainer />
       <CardContainer />
       {/* <h3>Welcome to React Social Card!</h3>
