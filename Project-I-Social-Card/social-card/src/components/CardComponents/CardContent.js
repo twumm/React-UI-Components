bc2c4f1c736aab86605366c4618962fa30f0913a
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-27 13:48:18 +0000

    feature: Create CardContent component with its elements

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent({ title, content, link }) {
+  return (
+    <div>
+      <h4>{title}</h4>
+      <p>{content}</p>
+      <p id="link">{link}</p>
+    </div>
+  );
+}
+
+export default CardContent;
