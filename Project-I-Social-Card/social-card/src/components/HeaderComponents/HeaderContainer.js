cefcdf720894e4b368e8bf1ab2db2648e08c9744
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-27 12:34:11 +0000

    feature: Import and add the HeaderContent component Put HeaderTitle and HeaderContent in one div

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ImageThumbnail from './ImageThumbnail';
 import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
 import './Header.css';
+
+const content = "Let's learn React by building simple interfaces with component. Don't try to overthink it, just keep it simple and have fun. Once you feel comfortable using components you are well on your way to mastering React!";
 
 function HeaderContainer() {
   return (
     <div className='header-container'>
       <ImageThumbnail imageSrc="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" />
-      <HeaderTitle name="Lambda School" username="LambdaSchool" date={Date.now()} />
+      <div className="header-content">
+        <HeaderTitle name="Lambda School" username="LambdaSchool" date={Date.now()} />
+        <HeaderContent content={content} />
+      </div>
     </div>
   )
 }
