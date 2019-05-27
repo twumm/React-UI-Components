09d3cfa4787459236a1ef3bcb68f5e7509130991
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-27 17:56:22 +0000

    feature: Set showcount variable on comment and mail formats

--- Project-I-Social-Card/social-card/src/components/FooterComponents/FooterContainer.js
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/FooterContainer.js
@@ -2,15 +2,15 @@ import React from 'react';
 import './Footer.css';
 import FooterLink from './FooterLink';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faComment, faRecycle, faHeart, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faComment, faRetweet, faHeart, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faComment, faRecycle, faHeart, faEnvelope);
+library.add(faComment, faRetweet, faHeart, faEnvelope);
 
 function Footer() {
   return (
     <div className='footer'>
       <FooterLink icon='comment' showcount="false" />
-      <FooterLink icon='recycle' />
+      <FooterLink icon='retweet' />
       <FooterLink icon='heart' />
       <FooterLink icon='envelope' showcount="false" />
     </div>
