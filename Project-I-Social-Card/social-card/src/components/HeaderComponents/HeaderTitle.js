732b283ea4294e8f1633e4300823af15291b46ae
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-27 12:22:30 +0000

    feature: Create a component that displays the Lambda School header text, @LambdaSchool handle and timestamp

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle({ name, username, date }) {
+  return (
+    <div className='header-title'>
+      <span>{name}</span>
+      <span>@{username}</span>
+      <span>{date}</span>
+    </div>
+  )
+}
+
+export default HeaderTitle;
