ba1d567939838099eb68a1c9a6215b578eda666d
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-27 12:05:04 +0000

    feature: Import and add the ImageThumbnail component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import ImageThumbnail from './ImageThumbnail';
 import './Header.css';
 
 function HeaderContainer() {
   return (
-    <div className='header-container'></div>
+    <div className='header-container'>
+      <ImageThumbnail imageSrc="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" />
+    </div>
   )
 }
 
