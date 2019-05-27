6782da8f08f7fa02f04f3d3d03db9dad85ab5ba4
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-27 13:25:13 +0000

    feature: Create CardBanner component that takes an image src as a prop

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Card.css';
+
+function CardBanner({ imageSrc }) {
+  return (
+    <div>
+      <img src={imageSrc} alt="Banner" />
+    </div>
+  );
+}
+
+export default CardBanner;
