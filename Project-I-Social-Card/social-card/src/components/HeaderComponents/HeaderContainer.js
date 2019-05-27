1360c7cad5572bdd91d0c1e7c1770ae35a2a5a0b
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-27 11:45:18 +0000

    feature: Create a div to house other header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderContainer() {
+  return (
+    <div className='header-container'></div>
+  )
+}
+
+export default HeaderContainer;
