d4c9986b5b3f21f4a11e7b69aec7d22b965eb065
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-27 13:20:03 +0000

    feature: Import and add CardBanner with an image src as a prop

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import CardBanner from './CardBanner';
 import './Card.css';
 
 function CardContainer() {
   return (
     <div className="card-container">
-
+      <CardBanner imageSrc="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png" />
     </div>
   );
 }
