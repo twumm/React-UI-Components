f1f7f122cc570f804b61e18dbf7a1ab74583afe0
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-27 12:21:40 +0000

    feature: Import and add the HeaderTitle component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ImageThumbnail from './ImageThumbnail';
+import HeaderTitle from './HeaderTitle';
 import './Header.css';
 
 function HeaderContainer() {
   return (
     <div className='header-container'>
       <ImageThumbnail imageSrc="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" />
+      <HeaderTitle name="Lambda School" username="LambdaSchool" date={Date.now()} />
     </div>
   )
 }
