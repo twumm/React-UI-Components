09126b09c4c43e1a6603d3704807699c02135646
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-27 13:44:30 +0000

    feature: Import and add CardContent with its props

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 import './Card.css';
+
+const cardContent = {
+  title: 'Get started with React',
+  content: 'React makes it painless to create interactive UIs. Design simple views for each state in your application',
+  link: 'reactjs.org'
+}
 
 function CardContainer() {
   return (
     <div className="card-container">
       <CardBanner imageSrc="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png" />
+      <CardContent title={cardContent.title} content={cardContent.content} link={cardContent.link} />
     </div>
   );
 }
