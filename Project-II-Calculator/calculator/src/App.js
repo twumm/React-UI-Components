359971008782ba92a0947a036894d24e4e310ae8
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-28 12:29:35 +0000

    feature: Display all button in the app

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 import NumberButton from './components/ButtonComponents/NumberButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 import './App.css';
 
 const App = () => {
@@ -8,10 +9,30 @@ const App = () => {
     <div className="app-container">
       <CalculatorDisplay display={0} />
       <div className='row'>
-        <NumberButton value={1} buttonStyle='number-button' />
-        <NumberButton value={2} buttonStyle='number-button' />
-        <NumberButton value={3} buttonStyle='number-button' />
-        <NumberButton value={'x'} buttonStyle='symbol-button' />
+        <ActionButton text={'clear'} buttonStyle='clear-button' />
+        <NumberButton text={'/'} buttonStyle='symbol-button' />
+      </div>
+      <div className='row'>
+        <NumberButton text={7} buttonStyle='number-button' />
+        <NumberButton text={8} buttonStyle='number-button' />
+        <NumberButton text={9} buttonStyle='number-button' />
+        <NumberButton text={'x'} buttonStyle='symbol-button' />
+      </div>
+      <div className='row'>
+        <NumberButton text={4} buttonStyle='number-button' />
+        <NumberButton text={5} buttonStyle='number-button' />
+        <NumberButton text={6} buttonStyle='number-button' />
+        <NumberButton text={'-'} buttonStyle='symbol-button' />
+      </div>
+      <div className='row'>
+        <NumberButton text={1} buttonStyle='number-button' />
+        <NumberButton text={2} buttonStyle='number-button' />
+        <NumberButton text={3} buttonStyle='number-button' />
+        <NumberButton text={'-'} buttonStyle='symbol-button' />
+      </div>
+      <div className='row'>
+        <ActionButton text={0} buttonStyle='zero-button' />
+        <NumberButton text={'='} buttonStyle='symbol-button' />
       </div>
       
     </div>
