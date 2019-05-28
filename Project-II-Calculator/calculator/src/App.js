aff3da737c4bf44aafcada9029d3bb78a40ffa81
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-28 12:19:14 +0000

    feature: Add some NumberButtons and style the div

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import NumberButton from './components/ButtonComponents/NumberButton';
 import './App.css';
 
 const App = () => {
   return (
     <div className="app-container">
       <CalculatorDisplay display={0} />
+      <div className='row'>
+        <NumberButton value={1} buttonStyle='number-button' />
+        <NumberButton value={2} buttonStyle='number-button' />
+        <NumberButton value={3} buttonStyle='number-button' />
+        <NumberButton value={'x'} buttonStyle='symbol-button' />
+      </div>
+      
     </div>
   );
 };
