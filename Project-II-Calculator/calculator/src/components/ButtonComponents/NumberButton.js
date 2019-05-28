21ebdd248c8c409023a31009f40b0b00e2acc113
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-28 12:38:20 +0000

    refactor: Change button element

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Button.css';
 
-export default function NumberButton({ value, buttonStyle }) {
+export default function NumberButton({ text, buttonStyle }) {
 
   return (
-    <button className={buttonStyle}>{value}</button>
+    <button className={buttonStyle}>{text}</button>
   );
 }
