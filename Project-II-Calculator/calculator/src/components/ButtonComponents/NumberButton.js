8ca9d54d8c66fa5b6c68e04dac11b2d03054e6d1
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-28 14:47:57 +0000

    feature: Add setTempNumber or getTotal prop

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './Button.css';
 
-export default function NumberButton({ text, buttonStyle, calculateHandler }) {
+export default function NumberButton({ text, buttonStyle, setTempNumber, getTotal }) {
 
   return (
-    <input className={buttonStyle} value={text} onClick={(event) => calculateHandler(event.target.value)} readOnly />
+    <input
+      className={buttonStyle}
+      value={text}
+      onClick={(event) => (
+        setTempNumber ? setTempNumber(event.target.value) : getTotal(event.target.value)
+      )}
+      readOnly />
   );
 }
