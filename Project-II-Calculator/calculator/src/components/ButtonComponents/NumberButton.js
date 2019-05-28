e9b6fabeccbb5df47616725b4cc19c3d268d50a7
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-28 11:44:18 +0000

    feature: Reture a span containing a value to represent the buttons

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Button.css';
+
+export default function NumberButton({ value, buttonWidth }) {
+  
+  return (
+    <span className='number-button'>{value}</span>
+  );
+}
