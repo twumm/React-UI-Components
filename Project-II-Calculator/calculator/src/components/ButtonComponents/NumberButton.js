285cca41f17e7b2fd398545fbb857c35a1ad2acc
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-28 11:47:06 +0000

    feature: If width of button is given, set the width with that else use 25%

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './Button.css';
 
 export default function NumberButton({ value, buttonWidth }) {
-  
+  // If buttonWidth is provided, set width, else use default width
+  const buttonStyle = {
+    width: buttonWidth ? buttonWidth : '25%', 
+  }
+
   return (
-    <span className='number-button'>{value}</span>
+    <span className='number-button' style={buttonStyle}>{value}</span>
   );
 }
