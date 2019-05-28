b41c673e3c56ed0ba016db97f5372164bb8c5d81
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-28 12:37:40 +0000

    feature: Return a button which accepts props from the parent component

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Button.css';
+
+export default function ActionButton({ text, buttonStyle }) {
+
+  return (
+    <button className={buttonStyle}>{text}</button>
+  );
+}
