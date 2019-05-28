56a9dc512f81277bbe9b52d452ea7be3830c3913
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-28 11:37:00 +0000

    feature: Import and initialize the CalculatorDisplay in App

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 import './App.css';
 
 const App = () => {
   return (
     <div className="app-container">
-      
+      <CalculatorDisplay display={0} />
     </div>
   );
 };
