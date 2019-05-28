7d1e022cd5e226d3868684656d2be4169390f8b5
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-28 11:33:30 +0000

    feature: Create calculator display and style it up

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+export default function CalculatorDisplay({ display }) {
+  return (
+    <div className="calculator-display">
+      <h1>{display}</h1>
+    </div>
+  )
+}
